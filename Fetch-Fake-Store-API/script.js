199ad48559414bcb7c01aa07743dd37c31ebc496
Douglas Obara <douglasobara97@gmail.com>
2022-06-15 23:44:40 +0300

    Fetch data from API and display data into browser.

---
+++ Fetch-Fake-Store-API/script.js
@@ -0,0 +1,21 @@
+fetch("https://fakestoreapi.com/products")
+  .then((data) => data.json())
+  .then((completeData) => {
+    // console.log(completeData[7].image);
+    let data1 = "";
+    completeData.map((values) => {
+      data1 += `
+      <div class="card">
+        <h1 class="title">${values.title}</h1>
+        <img
+          src=${values.image}
+          alt=${values.image}
+        />
+        <p>${values.description}</p>
+        <p class="category">${values.category}</p>
+        <p class="price">${values.price}</p>
+      </div>
+    `;
+    });
+    document.getElementById("cards").innerHTML = data1;
+  });
